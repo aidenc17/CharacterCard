4858d004ff140140b063b07e2361e897205bdff8
aiden cox <cox.378@wright.edu>
2025-02-13 13:35:14 -0500

    added the rocket image

--- app/src/main/java/com/example/charactercard/MainActivity.kt
+++ app/src/main/java/com/example/charactercard/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.charactercard.ui.theme.CharacterCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +26,56 @@ class MainActivity : ComponentActivity() {
         setContent {
             CharacterCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    CharacterCard(
+
+                        //modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
     }
 }
+@Composable
+fun CardType(){
+
+}
+@Composable
+fun CardTextStats(){
+
+}
+@Composable
+fun TitleCost(){
+
+}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CharacterCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        TitleCost()
+        Image(
+            painter = painterResource(id = R.drawable.screenshot_2025_02_13_at_12_44_04pm),
+            contentDescription = "Rocket",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(8.dp)
+        )
+        CardType()
+        CardTextStats()
+    }
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CharacterCardPreview() {
     CharacterCardTheme {
-        Greeting("Android")
+        CharacterCard()
     }
 }
